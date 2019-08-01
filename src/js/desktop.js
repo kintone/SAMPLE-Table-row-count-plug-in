7f523ea2c845c84230a335d98572d33b0a80dd63
Niki <43398321+niki-cy@users.noreply.github.com>
2019-08-01 17:15:21 +0900

    Fixed indent spacing

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -6,27 +6,27 @@
  * Licensed under the MIT License
  */
 (function(PLUGIN_ID) {
-    'use strict';
+  'use strict';
 
-    // Get plug-in configuration settings
-    var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
-    // Get each setting
-    if (!CONFIG) {
-        return false;
-    }
+  // Get plug-in configuration settings
+  var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
+  // Get each setting
+  if (!CONFIG) {
+    return false;
+  }
 
-    var TABLEROWS = CONFIG.table_row; // Field code of the table
-    var ROWCOUNT = CONFIG.row_count; // Field code of the number field
+  var TABLEROWS = CONFIG.table_row; // Field code of the table
+  var ROWCOUNT = CONFIG.row_count; // Field code of the number field
 
-    //The event triggers when the save button is clicked on the record create or edit page
-    kintone.events.on(["app.record.create.submit", "app.record.edit.submit"], function(eventobj) {
-        //Count the number of rows in the table
-        var num_of_rows = eventobj.record[TABLEROWS].value.length;
+  // The event triggers when the save button is clicked on the record create or edit page
+  kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], function(eventobj) {
+    // Count the number of rows in the table
+    var num_of_rows = eventobj.record[TABLEROWS].value.length;
 
-        //Set a new value in a field, listed in the event object
-        eventobj.record[ROWCOUNT].value = num_of_rows;
+    // Set a new value in a field, listed in the event object
+    eventobj.record[ROWCOUNT].value = num_of_rows;
 
-        //Return the event object, so that Kintone will use this new data
-        return eventobj;
-    });
+    // Return the event object, so that Kintone will use this new data
+    return eventobj;
+  });
 })(kintone.$PLUGIN_ID);
